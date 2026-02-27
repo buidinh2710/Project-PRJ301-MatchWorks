d34915a3036a94ddf4852765465404571745b174
linhlinhh129 <lethithuylinhtl12@gmail.com>
2026-02-27 16:45:25 +0700

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author dungn
+ */
+public class Customer {
+    private String customerID;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Customer() {
+    }
+
+    public Customer(String customerID, String name, String address, String phone) {
+        this.customerID = customerID;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+}
